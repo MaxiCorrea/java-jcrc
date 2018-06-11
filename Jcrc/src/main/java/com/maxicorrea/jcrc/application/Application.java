0a2a1db60653b3c4597cde5d4b8528d1446e97df
Maximiliano Correa <jmaxicorrea@gmail.com>
2018-06-11 04:13:27 -0300

    format

--- Jcrc/src/main/java/com/maxicorrea/jcrc/application/Application.java
+++ Jcrc/src/main/java/com/maxicorrea/jcrc/application/Application.java
@@ -4,7 +4,6 @@ import javax.swing.SwingUtilities;
 import com.maxicorrea.jcrc.models.JCrc;
 import com.maxicorrea.jcrc.views.JCrcView;
 
-
 public class Application {
 
   public static void main(String[] args) {
